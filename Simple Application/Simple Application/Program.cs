e7d7c2a96eb6db4bd91a780cf4b13e1445634e47
Viktor Yelenetskyi <ipz223_yevv@student.ztu.edu.ua>
2024-02-05 12:30:52 +0200

    Added Simple C# Application

---
+++ Simple Application/Simple Application/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SimpleApplication
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Program calculator = new Program();
+            List<float> numbers = calculator.UserInput();
+
+            float sum = calculator.SumOfNumbers(numbers[0], numbers[1]);
+
+            Console.WriteLine($"Sum of {numbers[0]} and {numbers[1]} is: {sum}");
+        }
+
+        public float SumOfNumbers(float firstNumber, float secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        public List<float> UserInput()
+        {
+            List<float> result = new List<float>();
+
+            for (int i = 0; i < 2; i++)
+            {
+                Console.Write($"Enter the {i + 1} number: ");
+                float number = float.Parse(Console.ReadLine()!);
+                result.Add(number);
+            }
+
+            return result;
+        }
+    }
+}
